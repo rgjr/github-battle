e1569ee23cae95e9083a7192c9912eea6265f3a4
Raul Gutierrez <rdgutierrez@gmail.com>
2017-04-24 13:54:36 -0700

    added playerpreview, results, pulling info from github and displaying 'score' stats

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,7 @@
-var React     = require('react');
-var PropTypes = require('prop-types');
+var React         = require('react');
+var PropTypes     = require('prop-types');
+var Link          = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -75,6 +77,7 @@ class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset  = this.handleReset.bind(this);
   }
 
   handleSubmit(id, username) {
@@ -87,9 +90,22 @@ class Battle extends React.Component {
     });
   }
 
+  handleReset(id) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name']   = '';
+      newState[id + 'Image']  = null;
+
+      return newState;
+    })
+  }
+
   render() {
-    var playerOneName = this.state.playerOneName;
-    var playerTwoName = this.state.playerTwoName;
+    var match           = this.props.match;
+    var playerOneName   = this.state.playerOneName;
+    var playerOneImage  = this.state.playerOneImage;
+    var playerTwoName   = this.state.playerTwoName;
+    var playerTwoImage  = this.state.playerTwoImage;
 
     return (
       <div>
@@ -101,13 +117,46 @@ class Battle extends React.Component {
               onSubmit={this.handleSubmit}
             />}
 
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}>
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}>
+                Reset
+              </button>
+            </PlayerPreview>}
+
           {!playerTwoName &&
             <PlayerInput
               id='playerTwo'
               label='Player Two'
               onSubmit={this.handleSubmit}
             />}
+
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerTwo')}>
+                Reset
+              </button>
+            </PlayerPreview>}
         </div>
+        {playerOneImage && playerTwoImage &&
+          <Link
+            className='button'
+            to={{
+              pathname: match.url + '/results',
+              search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName,
+            }}>
+            Battle
+          </Link>
+        }
       </div>
     )
   }
---
+++ app/components/Results.js
@@ -0,0 +1,119 @@
+var React         = require('react');
+var PropTypes     = require('prop-types');
+var queryString   = require('query-string');
+var api           = require('../utils/api');
+var Link          = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
+var PropTypes     = require('prop-types');
+
+function Profile (props) {
+  var info = props.info;
+
+  return (
+    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}></a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired,
+}
+
+function Player (props) {
+  return (
+    <div>
+      <h1 className='header'>{props.label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <Profile info={props.profile}/>
+    </div>
+  )
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+class Results extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    var players = queryString.parse(this.props.location.search);
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function (results) {
+      if (results === null) {
+        return this.setState(function () {
+          return {
+            error: 'error: check both users exist on github',
+            loading: false,
+          }
+        })
+      }
+
+      this.setState(function () {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      })
+    }.bind(this));
+  }
+  render() {
+    var error   = this.state.error;
+    var winner  = this.state.winner;
+    var loser   = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+    return (
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+      </div>
+    )
+  }
+}
+
+module.exports = Results;
